85d80187057731ab9546789e30e8eec919981b05
butlerx <cianbutlerx@gmail.com>
2017-03-26 12:14:00 +0100

    comment parker class

--- Parking.java
+++ Parking.java
@@ -228,6 +228,12 @@ class Car {
   }
 }
 
+/**
+ * Wait Manager class for queueing cars
+ *
+ * @version     1.0
+ * @since       1.0
+ */
 class WaitManager {
   private ArrayList<Car> waiting;
 
@@ -266,24 +272,46 @@ class WaitManager {
   }
 }
 
+/**
+ * Parker class for removeing cars from queue and parking them int the car Park
+ *
+ * @version   1.0
+ * @since     1.0
+ */
 class Parker extends Thread {
   private WaitManager queue;
   private boolean start;
   private CarPark park;
 
+  /**
+   * Constructor
+   *
+   * @param queue (required) the queue to take cars from
+   * @param park (required) the Car park to park in
+   */
   public Parker (WaitManager queue, CarPark park) {
     this.queue = queue;
     this.park = park;
   }
 
+  /**
+   * Restart the process in thread
+   */
   public void restart () {
     this.start = true;
   }
 
+  /**
+   * stop the process in the thread
+   */
   public void kill () {
     this.start = false;
   }
 
+  /**
+   * Override the threads run method
+   * Park car from queue
+   */
   @Override
   public void run () {
     this.start = true;
@@ -296,12 +324,20 @@ class Parker extends Thread {
   }
 }
 
+/**
+ * Clock class for keeping track of time in the carpark
+ *
+ * @version   1.0
+ * @since     1.0
+ */
 class Clock extends Thread {
   private CarPark carPark;
   private boolean start;
 
   /**
    * Constructor
+   *
+   * @param c (required) The car park to track time in
    */
   public Clock (CarPark c) {
     carPark = c;
